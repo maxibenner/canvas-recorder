35fc45100e9a3cde3924c3d18c1ba7428d716686
Maximilian Benner <maxibenner@gmail.com>
2021-05-12 14:56:22 -0700

    first commit

---
+++ helpers.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+var ffmpeg = require("fluent-ffmpeg");
+
+const removeDir = function (path, deleteParentDir) {
+	if (fs.existsSync(path)) {
+		const files = fs.readdirSync(path);
+
+		if (files.length > 0) {
+			files.forEach(function (filename) {
+				if (fs.statSync(path + "/" + filename).isDirectory()) {
+					removeDir(path + "/" + filename);
+				} else {
+					fs.unlinkSync(path + "/" + filename);
+				}
+			});
+			if (deleteParentDir !== false) {
+				fs.rmdirSync(path);
+			}
+		} else {
+			if (deleteParentDir !== false) {
+				fs.rmdirSync(path);
+			}
+		}
+	} else {
+		console.log("Directory path not found.");
+	}
+};
+exports.removeDir = removeDir;
+
+async function dataUriToDisk(data, path) {
+	//const fileName = path.split('/')
+	fs.readdir(path, (err, files) => {
+		fs.writeFile(
+			`${path}/${files.length}.png`,
+			data.split(";base64,").pop(),
+			{ encoding: "base64" },
+			(err) => {
+				return;
+			}
+		);
+	});
+}
+exports.dataUriToDisk = dataUriToDisk;
+
+async function pngsToMp4(inputPath, outputPath, fps, duration) {
+	// Get path for first image
+	const path = inputPath.replace("%d", "1");
+
+	// Get image dimensions
+	await new Promise((resolve, reject) => {
+		ffmpeg(path).ffprobe(0, (err, data) => {
+			if (data) {
+				const width = data.streams[0].width;
+				const height = data.streams[0].height;
+
+				//Convert pngs to video
+				ffmpeg(inputPath)
+					.fps(fps)
+					.outputOptions("-pix_fmt yuv420p")
+					.size(`${width}x${height}`)
+					.videoBitrate("8000k", true)
+					.on("end", () => {
+						console.log("[Server] File has been converted successfully");
+						resolve();
+					})
+					.save(outputPath);
+			} else {
+				reject();
+			}
+		});
+	});
+
+	return;
+}
+exports.pngsToMp4 = pngsToMp4;
---
+++ index.js
@@ -0,0 +1,100 @@
+// dependencies
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+const helpers = require("./helpers");
+
+const port = 5000;
+app.use(cors());
+app.use("/tmp", express.static("tmp"));
+
+app.listen(port);
+
+// Clear tmp directory on server start
+helpers.removeDir("./tmp", false);
+
+app.post("/add_frame", function (req, res) {
+	let dropbox = "";
+
+	req.on("data", (chunk) => {
+		dropbox += chunk.toString();
+	});
+
+	req.on("end", () => {
+		const body = JSON.parse(dropbox);
+
+		if (!body.data || !body.sessionId) {
+			res.send({
+				code: 400,
+				message: "Missing data",
+			});
+		}
+
+		// Create session dir and save to disk
+		if (fs.existsSync(`./tmp/${body.sessionId}`)) {
+			helpers.dataUriToDisk(body.data, `tmp/${body.sessionId}`);
+		} else {
+			fs.mkdir(`./tmp/${body.sessionId}`, () => {
+				helpers.dataUriToDisk(body.data, `tmp/${body.sessionId}`);
+			});
+		}
+
+		res.send({ code: 200, message: "Frame added to batch" });
+	});
+});
+
+app.get("/process", async function (req, res) {
+	const query = req.query;
+
+	if (!query.sessionId) {
+		res.send({
+			code: 400,
+			message: "So session id specified",
+			url: null,
+		});
+		if (!fs.existsSync(`./tmp/${query.sessionId}`)) {
+			res.send({
+				code: 400,
+				message: "No such session",
+				url: null,
+			});
+		}
+	}
+
+	helpers
+		.pngsToMp4(
+			`./tmp/${query.sessionId}/%d.png`,
+			`./tmp/${query.sessionId}/${query.sessionId}.mp4`,
+			query.fps,
+			query.duration
+		)
+		.then(() => {
+			// Set delete timer
+			setTimeout(() => {
+				helpers.removeDir(`./tmp/${query.sessionId}`);
+				console.log("[Server] User directory has been deleted.");
+			}, 20000);
+
+			res.send({
+				code: 200,
+				message: "Converted to mp4",
+				url: `./tmp/${query.sessionId}/${query.sessionId}.mp4`,
+			});
+		})
+		.catch(() => {
+			// Set delete timer
+			setTimeout(() => {
+				helpers.removeDir(`./tmp/${query.sessionId}`);
+				console.log("[Server] User directory has been deleted.");
+			}, 20000);
+
+			res.send({
+				code: 400,
+				message: "Problem converting images",
+				url: null,
+			});
+		});
+});
+
+console.log(`Listening on port ${port}`);
